a54cdf204bb5ffaa4b2300c9cac6e388a9f122f6
Toni <toni.giorgino@gmail.com>
2023-10-14 23:42:34 +0200

    workaround conda -L

--- src/pycv/make.sh
+++ src/pycv/make.sh
@@ -1,8 +1,13 @@
 # Note that in conda libpython3xx is not found in the path returned by ldflags. IMHO it is a bug.
 # The workaround is to -L appropriately. Will be fixed here.
 
-export PLUMED_MKLIB_CFLAGS="$(python3-config --cflags --embed)  -Ipybind11-2.11.1/include"
-export PLUMED_MKLIB_LDFLAGS=$(python3-config --ldflags --embed)
+conda_fixup=${CONDA_PREFIX+-L$CONDA_PREFIX/lib}
+if [ ! -z "$conda_fixup" ]; then 
+  echo "CONDA_PREFIX is set. Assuming conda and enabling a workaround for missing -L in python3-config --ldflags --embed"
+fi
+
+export  PLUMED_MKLIB_CFLAGS="$(python3-config  --cflags --embed) -Ipybind11-2.11.1/include"
+export PLUMED_MKLIB_LDFLAGS="$(python3-config --ldflags --embed) $conda_fixup"
 
 echo PLUMED_MKLIB_CFLAGS=$PLUMED_MKLIB_CFLAGS
 echo PLUMED_MKLIB_LDFLAGS=$PLUMED_MKLIB_LDFLAGS
